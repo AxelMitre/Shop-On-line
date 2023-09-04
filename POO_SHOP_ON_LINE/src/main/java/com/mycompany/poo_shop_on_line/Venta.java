22f56a2c736f77597c162f3798ed3158528f9e98
AxelMitre <101562837+AxelMitre@users.noreply.github.com>
2023-09-04 15:56:12 -0300

    Update Venta.java

--- POO_SHOP_ON_LINE/src/main/java/com/mycompany/poo_shop_on_line/Venta.java
+++ POO_SHOP_ON_LINE/src/main/java/com/mycompany/poo_shop_on_line/Venta.java
@@ -1,50 +1,70 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * La clase "Venta" representa una transacción de venta en un sistema de tienda en línea.
+ * Cada venta tiene una lista de productos, un cliente asociado, un número de factura único y una fecha.
  */
+
+// Declaración del paquete donde se encuentra la clase
 package com.mycompany.poo_shop_on_line;
 
+// Importación de las clases LocalDate y ArrayList del paquete java.time y java.util respectivamente
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 /**
- *
- * @author CS09
+ * La clase "Venta" se utiliza para registrar y gestionar las ventas de productos a clientes.
  */
 public class Venta {
-    private ArrayList <Producto> listaProd;
-    private Cliente cliente;
-    private static int cantVenta = 1090;
-    private static int nroFact;
-    private LocalDate fecha;
-    
-    
+    private ArrayList<Producto> listaProd; // Lista de productos vendidos en esta venta
+    private Cliente cliente; // Cliente que realizó la compra
+    private static int cantVenta = 1090; // Contador de ventas (incrementado automáticamente)
+    private static int nroFact; // Número de factura único
+    private LocalDate fecha; // Fecha de la venta
 
-    public Venta(Cliente cliente,LocalDate fecha) {
+    /**
+     * Constructor de la clase "Venta".
+     *
+     * @param cliente El cliente que realiza la compra.
+     * @param fecha La fecha de la venta.
+     */
+    public Venta(Cliente cliente, LocalDate fecha) {
         this.cliente = cliente;
-        this.cantVenta++ ;
-        this.nroFact++;
+        this.cantVenta++;
+        this.nroFact++; // Incrementa el número de factura para cada nueva venta
         this.fecha = fecha;
     }
-    
-    public void agregarProducto(ArrayList<Producto> p){
+
+    /**
+     * Método para agregar productos a la lista de productos vendidos en esta venta.
+     *
+     * @param p La lista de productos a agregar.
+     */
+    public void agregarProducto(ArrayList<Producto> p) {
         this.listaProd = p;
     }
-    public double calcularTotal(){
+
+    /**
+     * Método para calcular el total a pagar por los productos vendidos en esta venta.
+     *
+     * @return El total a pagar por los productos.
+     */
+    public double calcularTotal() {
         double totalPrecio = 0;
-        for(Producto Prod : listaProd){
-            totalPrecio +=Prod.getPrecio();
+        for (Producto prod : listaProd) {
+            totalPrecio += prod.getPrecio();
         }
         return totalPrecio;
     }
-    public void imprimirFactura(){
-        System.out.println("numero de venta: "+cantVenta+
-                "|fecha: "+fecha);
-        System.out.println("productos");
-        for(Producto prod : listaProd){
-            System.out.println(listaProd);
-            System.out.println("total a pagar " +calcularTotal());
+
+    /**
+     * Método para imprimir una factura que muestra detalles de la venta, incluyendo el número de venta,
+     * la fecha, la lista de productos y el total a pagar.
+     */
+    public void imprimirFactura() {
+        System.out.println("Número de venta: " + cantVenta + " | Fecha: " + fecha);
+        System.out.println("Productos:");
+        for (Producto prod : listaProd) {
+            System.out.println(prod);
         }
+        System.out.println("Total a pagar: " + calcularTotal());
     }
 }
