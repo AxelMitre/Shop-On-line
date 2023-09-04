11b5e0d5de9a46d8d06fe226b96e34690a77aebc
AxelMitre <101562837+AxelMitre@users.noreply.github.com>
2023-09-04 15:35:46 -0300

    Update Pantalon.java

--- POO_SHOP_ON_LINE/src/main/java/com/mycompany/poo_shop_on_line/Pantalon.java
+++ POO_SHOP_ON_LINE/src/main/java/com/mycompany/poo_shop_on_line/Pantalon.java
@@ -1,24 +1,30 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * La clase Pantalon representa un producto de tipo pantalón con descripción, precio, talle y modelo.
  */
 package com.mycompany.poo_shop_on_line;
 
 /**
- *
- * @author CS09
+ * La clase Pantalon es una subclase de Producto que representa un pantalón.
  */
-public class Pantalon extends Producto{
-    private int talle;
-    private String modelo;
-    
+public class Pantalon extends Producto {
+    private int talle; // Talle del pantalón.
+    private String modelo; // Modelo del pantalón.
+
+    /**
+     * Constructor de la clase Pantalon.
+     *
+     * @param desc La descripción del pantalón.
+     * @param precio El precio del pantalón.
+     * @param t El talle del pantalón.
+     * @param m El modelo del pantalón.
+     */
     public Pantalon(String desc, double precio, int t, String m) {
-        super(desc, precio);
+        super(desc, precio); // Llama al constructor de la clase padre (Producto).
         this.talle = t;
-        this.modelo =  m;
+        this.modelo = m;
     }
 
+    // Métodos getter y setter para el talle.
     public int getTalle() {
         return talle;
     }
@@ -27,6 +33,7 @@ public class Pantalon extends Producto{
         this.talle = talle;
     }
 
+    // Métodos getter y setter para el modelo.
     public String getModelo() {
         return modelo;
     }
@@ -35,43 +42,24 @@ public class Pantalon extends Producto{
         this.modelo = modelo;
     }
 
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public int getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
-
-    
-    
+    // Implementación del método abstracto getPrecio de la clase Producto.
     public double getPrecio() {
         double precio = this.precio;
         if (this.modelo.equals("Oxford")) {
             precio = precio + (0.15 * precio);
-        }if (this.modelo.equals("Skinny")) {
+        }
+        if (this.modelo.equals("Skinny")) {
             precio = precio + (0.25 * precio);
-        }if (this.modelo.equals("Recto")) {
+        }
+        if (this.modelo.equals("Recto")) {
             precio = precio - (0.15 * precio);
         }
         return precio;
     }
 
+    // Sobrescritura del método toString para mostrar la información del Pantalón.
     @Override
     public String toString() {
-        return "Pantalon: " + "Descripcion: "+ desc + " Talle: " + talle + " Modelo: " + modelo+ '.';
+        return "Pantalón: " + "Descripción: " + desc + " Talle: " + talle + " Modelo: " + modelo + '.';
     }
-
-
-    
-
-    
 }
