04c8c139829b28c92779f07b6187b8a05abd6d0c
AxelMitre <101562837+AxelMitre@users.noreply.github.com>
2023-09-04 14:50:37 -0300

    Update NewClass.java

--- POO_SHOP_ON_LINE/src/main/java/com/mycompany/poo_shop_on_line/NewClass.java
+++ POO_SHOP_ON_LINE/src/main/java/com/mycompany/poo_shop_on_line/NewClass.java
@@ -1,33 +1,42 @@
-
 package com.mycompany.poo_shop_on_line;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
+// Bloque principal
 
+// Esta es la clase principal del programa.
 public class NewClass {
     public static void main(String[] args) {
         Venta ventas = null;
         cagarVenta(ventas);
     }
 
+    // Este método se utiliza para cargar una venta (Venta) e interactuar con el usuario.
     private static void cagarVenta(Venta ventas) {
         Scanner lector = new Scanner(System.in);
         Cliente cliente = null;
-        ArrayList<Producto> producto =new ArrayList<>();
+        ArrayList<Producto> producto = new ArrayList<>();
+        
+        // Cargar información del cliente.
         cargarCliente(cliente, lector);
+        
         System.out.println("Cuantos productos quieres ingresar: ");
         int res = lector.nextInt();
+        
+        // Cargar información del producto.
         for (int i = 0; i < res; i++) {
             cargarProducto(producto, lector);
         }
+        
         LocalDate hoy = LocalDate.now();
         ventas = new Venta(cliente, hoy);
         ventas.agregarProducto(producto);
         ventas.imprimirFactura();
     }
 
+    // Este método carga la información del cliente.
     private static void cargarCliente(Cliente cliente, Scanner lector) {
         System.out.println("Datos del cliente:");
         System.out.println("Cual es tu nombre:");
@@ -39,6 +48,7 @@ public class NewClass {
         cliente = new Cliente(nom, ape, Dni);
     }
 
+    // Este método carga la información del producto según la elección del usuario.
     private static void cargarProducto(ArrayList<Producto> producto, Scanner lector) {
         System.out.println("Datos del Producto");
         System.out.println("Que tipo de producto busca\n"
@@ -59,24 +69,26 @@ public class NewClass {
         }
     }
 
+    // Este método carga información para un producto "Remera".
     private static void MostrarRemera(Scanner lector, ArrayList<Producto> producto) {
-        System.out.println("describe la remera buscas");
+        System.out.println("Describe la remera que buscas");
         String des = lector.next();
         double pres = 7500.00;
-        System.out.println("Que talla busca: S, M, L, XL");
+        System.out.println("Qué talla buscas: S, M, L, XL");
         String ta = lector.next();
         Remera remera = new Remera(des, pres, ta);
         producto.add(remera);
     }
 
+    // Este método carga información para un producto "Pantalón".
     private static void MostrarPantalones(Scanner lector, ArrayList<Producto> producto) {
-        System.out.println("describe que tipo de pantalon busca ");
+        System.out.println("Describe qué tipo de pantalón buscas");
         String des = lector.next();
         double pres = 7500.00;
-        System.out.println("Que talla busca: 4, 5");
+        System.out.println("Qué talla buscas: 4, 5");
         int ta = lector.nextInt();
         String mo = null;
-        System.out.println("Que tipo de modelo de pantalon busca \n"
+        System.out.println("Qué tipo de modelo de pantalón buscas \n"
                 + "1) Oxford \n"
                 + "2) Skinny \n"
                 + "3) Recto ");
@@ -96,13 +108,13 @@ public class NewClass {
         producto.add(pantalon);
     }
 
+    // Este método carga información para un producto "Accesorio".
     private static void MostrarAccesorios(Scanner lector, ArrayList<Producto> producto) {
-        //(String desc,double precio, String m, int g)
-        System.out.println("Describe el Accesorio de busca: ");
+        System.out.println("Describe el Accesorio que buscas: ");
         String des = lector.next();
         double pres = 8500.00;
         String ma = null;
-        System.out.println("Que tipo de materia quieres que tenga: \n"
+        System.out.println("Qué tipo de material quieres que tenga: \n"
                 + "1) Acero \n"
                 + "2) Plata \n"
                 + "3) Oro");
@@ -118,9 +130,4 @@ public class NewClass {
                 ma = "Oro";
                 break;
         }
-        System.out.println("Cuantos Gramos de ese material quieres que tenga: ");
-        int gra = lector.nextInt();
-        Accesorio accesorio = new Accesorio(des, pres, ma, gra);
-        accesorio.aduana();
-    }
-}
+        System.out.println("Cuantos gramos de ese material quieres que tenga: ");
